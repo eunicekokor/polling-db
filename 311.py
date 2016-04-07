31dd6225cb9bfcea5fb030c6a00caf206f531e6a
Ubuntu <ubuntu@ip-172-30-1-80.ec2.internal>
2016-04-07 00:50:59 +0000

    added updated

--- 311.py
+++ 311.py
@@ -3,6 +3,7 @@
 import requests
 import time
 import redis
+import urllib3
 ####################
 # Info We Care About Retreiving from this fetch
 # latitude, longitude, street_name, zip_code, complaint_type, created_date
@@ -12,39 +13,53 @@ import redis
 
 def get_historical_complaints():
   polling = True
-  offset=50000
+  offset=0
+  counter=0
+  offset_iterator=0
   while polling:
     # query= "$where=created_date"
     query_url = "https://data.cityofnewyork.us/resource/i3j2-v52s.json?$limit=50000&$offset={}".format(offset)
     print "Queried: {}".format(query_url)
+    
+    # connect to Redis
+    conn = redis.Redis(db=0)
     initial = time.time()
-    response = requests.get(query_url)
-    response_dict = response.json()
+    response_dict = requests.get(query_url)
+    print "got here!"
+    print response_dict.status_code
+    response_dict = response_dict.json()
+    print "figureed out response dict"
+    # response_dict = response.json()
     count = len(response_dict)
     if count <= 1:
       polling = False
-
+    keys = conn.keys("complaintID:*")
     for result in response_dict:
       # for each complaint, log into redis
       # filtering for specific months
       # key: jan('-01-'), april('-04-'), july ('-07-'), october ('-10-')
       date = result['created_date']
+      unique_key = result['unique_key']
       if '-01-' in date or '-04-' in date or '-07-' in date or '-10-' in date:
-        print "Adding {} \n".format(result)
-        conn = redis.Redis()
-        conn.set(result['unique_key'], result)
+        if unique_key not in keys:
+          counter += 1
+	  print "Adding new data to DB: {} \n".format(result)
+          insert_key = "complaintID:{}".format(result['unique_key'])
+          conn.set(insert_key, result)
 
     final = time.time()
     print "Total Time: {} seconds".format(final-initial)
-    print "Number of Entries: {} ".format(count)
+    print "Number of Entries: {} ".format(counter)
     offset += 50000
+    offset_iterator += 1
+    print "# of times script succesfully run: {}".format(offset_iterator)
 
 
 def get_realtime():
   # Setup Redis Connection
   conn = redis.Redis()
   # Load historical keys
-  historical_keys = conn.keys()
+  historical_keys = conn.keys("complaintID:*")
   # values = conn.mget(historical_keys)
 
   # Load "Today"'s json aka latest 50,000 complaints (maximum fetch size)
